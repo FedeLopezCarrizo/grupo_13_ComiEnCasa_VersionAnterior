7541bc1db52e9580a1789d74783085b38819093d
Diego250987 <diego.hernan.diaz@hotmail.com>
2020-09-09 19:15:14 -0300

    modifique app.js

--- site/app.js
+++ site/app.js
@@ -1,2 +1,14 @@
 const express = require('express');
+const {static} = require('express');
 const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+app.get('*', (res, req) => {
+    res.statusCode(400).send('No encontrada')
+});
+
+app.listen(3000, ()=> console.log('server corriendo'));
